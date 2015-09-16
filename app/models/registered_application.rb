a83aeb305b14b29b4e6c1bac0355f9a6a4c1a4d0
Ivor Padilla <ivorjpc@gmail.com>
2015-09-16 08:06:21 -0430

    Added validator to URLs

--- app/models/registered_application.rb
+++ app/models/registered_application.rb
@@ -1,8 +1,11 @@
 class RegisteredApplication < ActiveRecord::Base
 
-  validates :name, :url,
-              presence: true,
-              uniqueness: true
+  validates :name, presence: true, uniqueness: true
+
+  validates :url,
+            format: URI::regexp(%w(http https)),
+            presence: true,
+            uniqueness: true
 
   belongs_to :user
 end
