561f644194d8f70e7e1cf95c6c312558fe2e9699
Ivor Padilla <ivorjpc@gmail.com>
2015-09-18 14:21:44 -0430

    cleaned a line

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     match '/events', to: 'events#create', via: [:options]
     resources :events, only: [:create]
-
   end
 
 
